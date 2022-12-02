fd3bc6e3063b3a2dce4c29b2db2d872375401d26
Seamus Naaykens <snaaykens@gmail.com>
2022-12-02 00:14:07 -0700

    Created suggestion bar structure and the corresponsing changes to the homepage

---
+++ client/src/components/SuggestionBar/SuggestionBar.js
@@ -0,0 +1,38 @@
+
+function SuggestionBar() {
+    return(
+        <div className='suggestion-bar'>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>All</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>Music</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>TV</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>Movies</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>History</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>News</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>Radio</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>Watched</p>
+            </div>
+            <div>
+                <p className='suggestion-bar__content'>New to you</p>
+            </div>
+            <div className='suggestion-bar__container'>
+                <p className='suggestion-bar__content'>Recently Upload</p>
+            </div>
+        </div>
+    )
+}
+export default SuggestionBar
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,13 +1,17 @@
 import "./Homepage.scss"
 import Sidebar from "../../components/Sidebar/Sidebar"
+import SuggestionBar from "../../components/SuggestionBar/SuggestionBar"
 import VideoLibrary from "../../components/VideoLibrary/VideoLibrary"
 
 function Homepage() {
- return (
-    <div>
-      <Sidebar/>
-      <VideoLibrary/>
-    </div>
- )
+  return (
+    <main className="homepage">
+      <Sidebar />
+      <div>
+        <SuggestionBar />
+        <VideoLibrary />
+      </div>
+    </main>
+  )
 }
 export default Homepage 
